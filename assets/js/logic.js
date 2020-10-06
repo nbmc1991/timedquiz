745a9c4435c93806c8f16e3affa5b5658470e06a
nbmc1991 <nbmc1991@gmail.com>
2020-10-06 17:34:01 -0400

    index.html

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+let timer = document.getElementById("timer");
+let start = document.getElementById("start");
+let question = document.getElementById("question");
+let choiceA = document.getElementById("A");
+let choiceB = document.getElementById("B");
+let choiceC = document.getElementById("C");
+let progress = document.getElementById("proguess");
+let scoreContainer = document.getElementById("scoreContainer")
+
+ renderQuestion()
+
+ function renderQuestion(){
+    for( var i = 0; i<question.length; i++);
+    console.log("renderQestion");
+ }
---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+let questions = [{
+    question: "What does HTML stand for?",
+    choiceA: "only works on mac",
+    choiceB: "hyper text markup language",
+    choiceC: "hypo active language",
+    correct: "B",
+},
+{
+    question : "What is JavaScript?",
+    choiceA: "easy",
+    choiceB: "hard",
+    choiceC: "programming language",
+    correct: "C"
+},
+{
+    question : "What does WWW stand for?",
+    choiceA: "World Wide Web",
+    choiceB: "What went wrong",
+    choiceC: "none of the above",
+    correct: "A",
+
+}
+]
+
